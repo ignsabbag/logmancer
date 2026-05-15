914ed3204b4068b3353db3f888a796deb1dcf285
nacho <nachosabbag@gmail.com>
2026-05-15 17:34:39 -0300

    fix(release): polish app titles

--- logmancer-web/src/components/app.rs
+++ logmancer-web/src/components/app.rs
@@ -17,7 +17,7 @@ pub fn App() -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/logmancer-web.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Logmancer"/>
 
         // content for this welcome page
         <Router>
--- packaging/portable/windows/run-desktop.cmd
+++ packaging/portable/windows/run-desktop.cmd
@@ -3,4 +3,4 @@ set LEPTOS_OUTPUT_NAME=logmancer-web
 set LEPTOS_SITE_ROOT=%~dp0site
 if not exist "%~dp0logs" mkdir "%~dp0logs"
 set LOGMANCER_LOG_FILE=%~dp0logs\logmancer-desktop.log
-"%~dp0logmancer-desktop.exe" %*
+start "" "%~dp0logmancer-desktop.exe" %*
